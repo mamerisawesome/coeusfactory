6774dc98b0c373e30d646eedaef470432b749d65
mamerisawesome <atmalmer23@gmail.com>
2020-02-18 17:36:26 +0800

    create svg on coverage success

--- .bin/runCoverage.sh
+++ .bin/runCoverage.sh
@@ -1,9 +1,9 @@
 function runCoverage () {
     coverage run --source coeusfactory/ -m pytest tests &&
     coverage html &&
-    open htmlcov/index.html
+    open htmlcov/index.html &&
 
-    rm assets/coverage.svg &&
+    rm assets/coverage.svg >/dev/null &&
     coverage-badge -o assets/coverage.svg
 }
 
